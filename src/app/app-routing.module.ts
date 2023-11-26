1038f7487409d944db088dd17ab7e90e1456e001
lewy212 <huak@wp.pl>
2023-11-26 15:42:07 +0100

    dodano routingi oraz strone startowa (moze przerobic na logowanie czy cos)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserListComponent} from "./user-list/user-list.component";
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component: HomeComponent},
+  {path:'allUsers',component: UserListComponent},
+  {path:'',redirectTo:'/home',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { UserListComponent } from './user-list/user-list.component';
 import { UserListElementComponent } from './user-list/user-list-element/user-list-element.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
-    UserListElementComponent
+    UserListElementComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
