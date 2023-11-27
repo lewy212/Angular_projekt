ca888770846eac819f2d7e0fedc58f48bbbd90b1
lewy212 <huak@wp.pl>
2023-11-27 18:16:18 +0100

    naprawiony guard dla admina

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AdminComponent} from "./admin.component";
 import {UserListComponent} from "./user-list/user-list.component";
+import {authGuard} from "../auth.guard";
 
 const routes: Routes = [
   { path: '',component: AdminComponent,children:[
     { path: '', redirectTo: 'allUsers', pathMatch: 'full' },
-    {path: 'allUsers',component: UserListComponent}] },
+    {path: 'allUsers',component: UserListComponent,canActivate: [authGuard]}] },
 ];
 
 @NgModule({
