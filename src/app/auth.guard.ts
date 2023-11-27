fb890155bf3396de2d8f206002e3ddc925020346
lewy212 <huak@wp.pl>
2023-11-27 10:19:33 +0100

    naprawa serwisu autoryzujacego, juz dziala poprawnie

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -3,12 +3,11 @@ import {inject} from "@angular/core";
 import {UserService} from "./services/user.service";
 
 export const authGuard: CanActivateFn = (route, state) => {
-  if(inject(UserService).session && inject(UserService).session.id === 1)
+  if(inject(UserService).session && inject(UserService).session._id === 1)
   {
-    console.log("udalo sie")
     return true;
   }
-  console.log("Fail")
+
   inject(Router).navigateByUrl('/');
   return false;
 };
