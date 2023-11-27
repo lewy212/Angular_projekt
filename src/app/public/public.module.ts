fc66d635c4f652fa5848bf89efc5e95153e6f15b
lewy212 <huak@wp.pl>
2023-11-27 18:06:53 +0100

    import dodany

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -4,16 +4,18 @@ import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
 import {LandingModule} from "./landing/landing.module";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [
     PublicComponent
   ],
-  imports: [
-    CommonModule,
-    PublicRoutingModule,
-    LandingModule
-  ]
+    imports: [
+        CommonModule,
+        PublicRoutingModule,
+        LandingModule,
+        MatButtonModule
+    ]
 })
 export class PublicModule { }
